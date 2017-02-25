39dccf2508e69a511efe4b07e3f2dea0002647bd
Remco Peereboom <rpeere@yandex.com>
2017-02-25 10:32:12 +0100

    Fixed some documentation.

--- lib/ruby_on_rays/perspective_camera.rb
+++ lib/ruby_on_rays/perspective_camera.rb
@@ -1,6 +1,7 @@
 module RubyOnRays
   # PerspectiveCamera
   class PerspectiveCamera
+    # Initializes a new camera.
     def initialize(width, height)
       @width = width
       @height = height
@@ -13,6 +14,10 @@ module RubyOnRays
       @offset_y = -1.0 + 0.5 * @dy
     end
 
+    # Yields image samples with their corresponding rays.
+    # @yield [<Point2, Ray3>]
+    # @return [Enumerator<:each_with_ray>] if no block given.
+    # @return [self] f a block is given.
     def each_sample_with_ray
       unless block_given?
         return enum_for(:each_sample_with_ray) { @width * @height }
@@ -55,7 +60,7 @@ module RubyOnRays
     private
 
     # Converts a ChunkyPNG::Color to a RubyOnRays::Color
-    # @param color [ChunkyPNG::Color]
+    # @param chunky_color [ChunkyPNG::Color]
     # @return [RubyOnRays::Color]
     def chunky_to_color(chunky_color)
       r = chunky_color.r / 255.0
